b60ec8099f88ca5baf2f79a4cb7364447e32653b
Mateus Dal Bianco <mateus@dlbn.co>
2024-05-11 00:24:23 +0200

    feat: set up vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config'
 import webmanifest from 'astro-webmanifest'
 // @ts-expect-error for some reason is not getting the path properly
 import getHomeDocument from './src/utils/getHomeDocument.ts'
+import vercel from '@astrojs/vercel/static'
 
 export default defineConfig({
   site: 'https://photo.dlbn.co',
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   trailingSlash: 'ignore',
   integrations: [
     {
