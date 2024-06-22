ba8efa506e3514792a0cc742c21cd05b9e459983
Mateus Dal Bianco <mateus@dlbn.co>
2024-06-22 13:41:59 +0200

    chore(prismic): update types

---
+++ src/prismic.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import * as prismic from '@prismicio/client';
+import config from '../.prismic.json';
+import fetch from 'node-fetch';
+export const repositoryName = import.meta.env.PRISMIC_REPO || process.env.PRISMIC_REPO || config.prismicRepo;
+console.log({ repositoryName });
+// Update the Link Resolver to match your project's route structure
+export const linkResolver = doc => {
+    switch (doc.type) {
+        case 'homepage':
+            return '/';
+        case 'page':
+        default:
+            return `/${doc.uid || ''}`;
+    }
+};
+export const createClient = (config = {}) => {
+    const client = prismic.createClient(repositoryName, { ...config, fetch });
+    return client;
+};
